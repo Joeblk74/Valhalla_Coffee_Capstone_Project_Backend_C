430d6e2bb8ed1e5548f2bd9b7e9e4a9ce185d7f0
joeblk74 <joeblk@gmail.com>
2021-11-15 14:14:10 -0900

    Updated syntax to resolve pathing issues and connectivity to React front end

--- drf_jwt_capstone_backend/valhalla/urls.py
+++ drf_jwt_capstone_backend/valhalla/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from drf_jwt_capstone_backend.valhalla.models import RegisterUser
-from drf_jwt_capstone_backend.valhalla.views import Login
+# from drf_jwt_capstone_backend.valhalla.models import RegisterUser
+# from drf_jwt_capstone_backend.valhalla.views import Login
 from valhalla import views 
 
 urlpatterns = [
